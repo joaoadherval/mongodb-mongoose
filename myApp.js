5a0d247be06e506eb4c5de34d24d536372c93a2e
Joao Adherval <joaoadherval@gmail.com>
2021-08-19 22:11:11 -0300

    creating person model

--- myApp.js
+++ myApp.js
@@ -3,6 +3,16 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+const Person = mongoose.model('Person', personSchema);
+
 let Person;
 
 const createAndSavePerson = (done) => {
